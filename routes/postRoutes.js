849be5ee17184cbecb6750c2492d451b2760bcef
Hoxtygen <hoxtygen@live.com>
2019-07-09 13:33:19 +0100

    Delete post
    Should post need to be deleted, it will need an endpoint to do that; that's the essence of this commit. A DELETE enpoint has been added and given a valid id, will delete the specified id. In cases where the id is incorrect, it throws an error showing that the id is non-existent.

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -18,7 +18,6 @@ route.get('/', async (req, res) => {
 route.get('/:id', async (req, res) => {
     try {
         const post = await postsModel.findById(parseInt(req.params.id, 10));
-        console.log(post)
         if (post.length) {
             return res.status(200).json({
                 post
@@ -31,6 +30,25 @@ route.get('/:id', async (req, res) => {
     } catch (error) {
         res.status(500).json({
             error: "The post information could not be retrieved."
+        })
+    }
+});
+
+route.delete('/:id', async (req, res) => {
+    try {
+        const post = await postsModel.remove(parseInt(req.params.id, 10));
+        if (post) {
+            return res.status(200).json({
+                message: 'post successfully deleted'
+            });
+        } else {
+           return res.status(404).json({
+                message: "The post with the specified ID does not exist."
+            })
+        }
+    } catch (error) {
+        res.status(500).json({
+            error: "The post could not be removed"
         })
     }
 })
