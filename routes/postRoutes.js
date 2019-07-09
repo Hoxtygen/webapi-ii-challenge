080370c956f7a98b4c963fd877904a65d749e069
Hoxtygen <hoxtygen@live.com>
2019-07-09 13:24:42 +0100

    Get a single post
    An endpoint to get a single post was added. With a valid id, a post can be fetched, otherwise it returns error.

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -4,7 +4,7 @@ const postsModel = require('../data/db');
 const route = express.Router()
 
 
-route.get('/', async(req, res) => {
+route.get('/', async (req, res) => {
     try {
         const posts = await postsModel.find(req.query);
         res.status(200).json(posts);
@@ -15,6 +15,25 @@ route.get('/', async(req, res) => {
     }
 })
 
+route.get('/:id', async (req, res) => {
+    try {
+        const post = await postsModel.findById(parseInt(req.params.id, 10));
+        console.log(post)
+        if (post.length) {
+            return res.status(200).json({
+                post
+            });
+        } else {
+           return res.status(404).json({
+                message: "The post with the specified ID does not exist."
+            })
+        }
+    } catch (error) {
+        res.status(500).json({
+            error: "The post information could not be retrieved."
+        })
+    }
+})
 
 
 module.exports = route;
